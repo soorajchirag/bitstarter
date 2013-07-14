89a6c7e1198e5edb2ca0203fc8beee8547de2fee
Sundeep Singh <soorajchirag@gmail.com>
2013-07-14 09:44:00 +0000

    Reading html file now

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+
 app.get('/', function(request, response) {
-  response.send('Hello World - Bitstarter!');
+  var fs = require('fs');
+  var buf = fs.readFileSync('index.html');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
